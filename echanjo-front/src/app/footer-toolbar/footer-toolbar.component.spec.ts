56345c33b66351f5ad79060ee7d5b509768c9c71
Clement-Chalencon <clement.chalencon@le-campus-numerique.fr>
2021-03-13 21:09:27 +0100

    setup front project

---
+++ echanjo-front/src/app/footer-toolbar/footer-toolbar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FooterToolbarComponent } from './footer-toolbar.component';
+
+describe('FooterToolbarComponent', () => {
+  let component: FooterToolbarComponent;
+  let fixture: ComponentFixture<FooterToolbarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FooterToolbarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FooterToolbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
