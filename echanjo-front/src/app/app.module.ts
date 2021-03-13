42a992f4149af52b90321afd89503e402db99814
Clement-Chalencon <clement.chalencon@le-campus-numerique.fr>
2021-03-13 21:47:06 +0100

    footer and toolbar

--- echanjo-front/src/app/app.module.ts
+++ echanjo-front/src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderToolbarComponent } from './header-toolbar/header-toolbar.component';
 import { FooterToolbarComponent } from './footer-toolbar/footer-toolbar.component';
+import { MaterialModule } from './material.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderToolbarComponent,
-    FooterToolbarComponent
+    FooterToolbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
